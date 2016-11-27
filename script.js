dab2f1bf2628005ef632388f21ec9e42d69ea436
TheNewJavaman <gpizarro@javaman.net>
2016-11-27 06:01:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $(".logo, .link1").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
+
+  $(".link2, #btn4").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".s1").offset().top
+    }, 1000);
+  });
+
+  $(".link3").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".s2").offset().top
+    }, 1000);
+  });
+
+  $(".link4").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".s3").offset().top
+    }, 1000);
+  });
+
+});
