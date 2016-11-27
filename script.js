110f4c0977a7d5e9466d2794cfee1ab654dae4c2
TheNewJavaman <gpizarro@javaman.net>
2016-11-27 15:53:05 -0800

    Does this work?!?!?!?!?
    I sure hope so...

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-  $(".logo, .link1, #btn3, #btn4").click(function() {
+  $(".logo, .link1).click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
   });
 
-  $(".link2,  #btn2").click(function() {
+  $(".link2).click(function() {
     $('html, body').animate({
         scrollTop: $(".s1").offset().top
     }, 1000);
@@ -20,10 +20,6 @@ $(document).ready(function() {
     $('html, body').animate({
         scrollTop: $(".s3").offset().top
     }, 1000);
-  });
-
-  $("btn5").click(function() {
-    $(location).attr('href', 'contact.html')
   });
 
 });
