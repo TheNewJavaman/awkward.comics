b3b4f9522c1c6fddb6608da72d2acbaf4b18cc36
TheNewJavaman <gpizarro@javaman.net>
2016-11-27 16:49:36 -0800

    i think so now...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $(".logo, .link1").click(function() {
+  $(".logo, .link1, jumbutton").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
   });
 
