ecc351f0effc12c14c7d7361c94c45c26d30d450
TheNewJavaman <gpizarro@javaman.net>
2016-12-04 08:17:01 -0800

    Fade animation to comics.html

--- script.js
+++ script.js
@@ -30,25 +30,39 @@ $(document).ready(function() {
     var pair = vars[0].split("=");
     var latest = pair[1];
     if(latest>=parseInt(parseInt(currentindex)+2)){
+      var newImg = $("<img src='"+src+"' width='50%' height='50%' id='comicimg'/>").fadeTo(0,0);
+      $("#comicimg").fadeTo(500,0.5);
       $("#comic #comicimg").remove();
       $("#list #listp").remove();
-      $("#comic").append("<img src='"+src+"' width='50%' height='50%' id='comicimg'/>");
-      $("#list").append("<p class=\"paragraph\" id=\"listp\">"+parseInt(parseInt(currentindex)+2)+"/"+latest+"</p>");
+      $("#comic").append(newImg);
+      $("#list").append("<p class=\"paragraph\" id=\"listp\">"+parseInt(latest-(parseInt(currentindex)+2)+1)+"/"+latest+"</p>");
+      $("#comicimg").fadeTo(500,1);
+      if(src.includes(parseInt(latest-1))){
+        $("#back").fadeTo(300,0.2);
+      }
     }else{
       console.log("User is at the end of the comics list");
     };
+    $("#next").fadeTo(300,1);
   });
 
   $("#next").click(function() {
     var currentindex = $('#comicimg').attr("src").replace("comics/","").replace(".jpg","");
     var src = "comics/"+parseInt(currentindex-1)+".jpg";
     if(!(src.includes("-1"))){
+      var newImg = $("<img src='"+src+"' width='50%' height='50%' id='comicimg'/>").fadeTo(0,0);
+      $("#comicimg").fadeTo(500,0.5);
       $("#comic #comicimg").remove();
       $("#list #listp").remove();
-      $("#comic").append("<img src='"+src+"' width='50%' height='50%' id='comicimg'/>");
-      $("#list").append("<p class=\"paragraph\" id=\"listp\">"+parseInt(currentindex)+"/"+latest+"</p>");
+      $("#comic").append(newImg);
+      $("#list").append("<p class=\"paragraph\" id=\"listp\">"+parseInt(latest-(parseInt(currentindex))+1)+"/"+latest+"</p>");
+      $("#comicimg").fadeTo(500,1);
+      if(src.includes("0")){
+        $("#next").fadeTo(300,0.2);
+      }
     }else{
       console.log("User is at the end of the comics list");
     };
+    $("#back").fadeTo(300,1);
   });
 });
